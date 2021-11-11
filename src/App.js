41d756200f45ad4a202c1b1c25ad4b5f4d88ebc3
Cesar Huber <chuber@MacBook-Pro-de-Cesar.local>
2021-11-11 18:54:17 -0300

    cesar - inclusão revisão conjunta

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 import React from 'react';
-import { MainContainer, GlobalStyle } from './style'
+import { MainContainer, GlobalStyle, ContainerConteudoCentral, ContainerHeader, ContainerOrdenacao, ContainerCards } from './style'
 import LateralEsquerda from './components/lateralEsquerda/LateralEsquerda'
-import ConteudoCentral from './components/home/Home';
 import LateralDireita from './components/lateralDireita/LateralDireita'
+import Card from './components/card/Card'
+import listaProdutos from './data/produtos.json'
 
 export default class App extends React.Component {
 
   // Estado para comunicação entre Lateral Esquerda (Filtros) e Conteúdo Central (Produtos)
   state = {
+    produtos: listaProdutos,
     query: '',
     maxPrice: '',
     minPrice: '',
     sortingParameter: 'produto',
-    order: 'asc'
+    order: 'asc',
+    listaFiltrada: ''
   }
 
   updateQuery = (evento) => {
     this.setState({
       query: evento.target.value
-   })
+    })
   }
 
   updateMaxPrice = (evento) => {
     this.setState({
       maxPrice: evento.target.value
-   })
+    })
   }
 
   updateMinPrice = (evento) => {
     this.setState({
       minPrice: evento.target.value
-   })
+    })
   }
 
   render() {
-
+    // função para atualizar state lista-filtrada
     return (
       <MainContainer>
         <GlobalStyle />
-        <LateralEsquerda 
+        <LateralEsquerda
           query={this.state.query}
           maxPrice={this.state.maxPrice}
           minPrice={this.state.minPrice}
@@ -46,10 +49,42 @@ export default class App extends React.Component {
           updateMaxPrice={this.updateMaxPrice}
           updateMinPrice={this.updateMinPrice}
         />
-        <ConteudoCentral 
-          order={this.state.order}
-          sortingParameter={this.state.sortingParameter}
-        />
+        <ContainerConteudoCentral>
+          <ContainerHeader>
+            <h3>Quantidade de Produtos: {this.state.produtos.length} </h3>
+            <ContainerOrdenacao>
+              <label>Ordenação</label>
+              <select>
+                <option>Crescente</option>
+                <option>Decrescente</option>
+              </select>
+            </ContainerOrdenacao>
+          </ContainerHeader>
+          <hr />
+          <ContainerCards>
+            {this.state.produtos
+              .filter(item => {
+                return item.name.toLowerCase().includes(this.state.query.toLowerCase())
+              })
+              .filter(item => {
+                return this.state.minPrice === "" || item.value >= this.state.minPrice
+              })
+              .filter(item => {
+                return this.state.maxPrice === "" || item.value <= this.state.maxPrice
+              })
+              .sort((currentItem, nextItem) => {
+                switch (this.state.sortingParameter) {
+                  case "title":
+                    return this.state.order * currentItem.title.localeCompare(nextItem.title)
+                  default:
+                    return this.state.order * (currentItem.value - nextItem.value)
+                }
+              })
+              .map(item => {
+                return <Card key={item.id} item={item} />
+              })}
+          </ContainerCards>
+        </ContainerConteudoCentral>
         <LateralDireita />
       </MainContainer>
     )
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,9 +3,9 @@ import { CardContainer } from './style'
 
 export default function Card(props) {
     return <CardContainer>
-        <img src={props.produto.imageUrl} />
-        <h2>{props.produto.name}</h2>
-        <p>{props.produto.value.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})}</p>
+        <img src={props.item.imageUrl} />
+        <h2>{props.item.name}</h2>
+        <p>{props.item.value.toLocaleString('pt-BR', {style:'currency', currency:'BRL'})}</p>
         <button>Adicionar ao Carrinho</button>
     </CardContainer>
 }
